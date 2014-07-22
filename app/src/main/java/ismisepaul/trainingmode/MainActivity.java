5b4cb0ba8cf40147500e1dd890cbbcf17072792c
ismisepaul <pmccann1984@gmail.com>
2014-07-22 15:13:21 +0100

    Added all functionality at this time lock screen, auto-sync, and mobile data are configurable. Updated the icon

--- app/src/main/java/ismisepaul/trainingmode/MainActivity.java
+++ app/src/main/java/ismisepaul/trainingmode/MainActivity.java
@@ -6,17 +6,16 @@ import android.content.Context;
 import android.content.ContentResolver;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Switch;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.util.Log;
+import android.widget.ToggleButton;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-
-import static android.provider.Settings.Global.*;
 
 
 public class MainActivity extends Activity {
@@ -31,12 +30,18 @@ public class MainActivity extends Activity {
         * http://stackoverflow.com/questions/5773504/reenablekeyguard-not-working*/
         final KeyguardManager km = (KeyguardManager)getSystemService(Activity.KEYGUARD_SERVICE);
         final KeyguardManager.KeyguardLock lock = km.newKeyguardLock(KEYGUARD_SERVICE);
+        final Switch switch_lockScreen = (Switch) findViewById(R.id.switch_lockScreen);
+        final Switch switch_autoSync = (Switch) findViewById(R.id.switch_autoSync);
+        final Switch switch_mobileData = (Switch) findViewById(R.id.switch_mobileData);
+        final ToggleButton toggleBtn_mobileData =
+                (ToggleButton) findViewById(R.id.toggleBtn_everything);
 
-        Switch switch_lockScreen = (Switch) findViewById(R.id.switch_lockScreen);
-        if(lockScreenCheck()){
-            switch_lockScreen.setChecked(lockScreenCheck());
-        }
 
+        switch_lockScreen.setChecked(getLockScreenStatus(km));
+        switch_autoSync.setChecked(getAutoSyncStatus());
+        switch_mobileData.setChecked(getMobileDataStatus());
+
+        /*lock screen listener*/
         switch_lockScreen.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
@@ -48,11 +53,6 @@ public class MainActivity extends Activity {
         });
 
         /*Auto Sync */
-        Switch switch_autoSync = (Switch) findViewById(R.id.switch_autoSync);
-        if(getAutoSyncStatus()){
-            switch_autoSync.setChecked(getAutoSyncStatus());
-        }
-
         switch_autoSync.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
@@ -64,11 +64,6 @@ public class MainActivity extends Activity {
         });
 
         /*Mobile Data */
-        Switch switch_mobileData = (Switch) findViewById(R.id.switch_mobileData);
-        if(getMobileDataStatus()){
-            switch_mobileData.setChecked(getMobileDataStatus());
-        }
-
         switch_mobileData.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
@@ -79,9 +74,58 @@ public class MainActivity extends Activity {
             }
         });
 
+        /*All Settings */
+        toggleBtn_mobileData.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    setLockScreen(lock, Boolean.FALSE);
+                    setAutoSyncStatus(Boolean.FALSE);
+                    setMobileDataStatus(Boolean.FALSE);
+
+                    switch_lockScreen.setChecked(Boolean.FALSE);
+                    switch_autoSync.setChecked(Boolean.FALSE);
+                    switch_mobileData.setChecked(Boolean.FALSE);
+
+                } else {
+                    setLockScreen(lock, Boolean.TRUE);
+                    setAutoSyncStatus(Boolean.TRUE);
+                    setMobileDataStatus(Boolean.TRUE);
+
+                    switch_lockScreen.setChecked(Boolean.TRUE);
+                    switch_autoSync.setChecked(Boolean.TRUE);
+                    switch_mobileData.setChecked(Boolean.TRUE);
+                }
+            }
+
+           });
 
     }
 
+    @Override
+    public void onResume(){
+        /*IF the application is resumed check the status of the settings
+         * lockscreen, autosync and mobiledata */
+        super.onResume();
+
+        final Switch switch_lockScreen = (Switch) findViewById(R.id.switch_lockScreen);
+        final Switch switch_autoSync = (Switch) findViewById(R.id.switch_autoSync);
+        final Switch switch_mobileData = (Switch) findViewById(R.id.switch_mobileData);
+        final ToggleButton toggleBtn_mobileData =
+                (ToggleButton) findViewById(R.id.toggleBtn_everything);
+
+        //Check the status of autosync and mobile data and set the buttons appropriately
+        switch_autoSync.setChecked(getAutoSyncStatus());
+        switch_mobileData.setChecked(getMobileDataStatus());
+
+        /*On Resume check if all buttons are on/off and set the Full Training mode Toggle
+         * to on/off as appropriate */
+        if(!switch_lockScreen.isChecked() && !switch_autoSync.isChecked()
+                && !switch_mobileData.isChecked()){
+            toggleBtn_mobileData.setChecked(Boolean.TRUE);
+            Log.d("Full Training","set");
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -103,36 +147,13 @@ public class MainActivity extends Activity {
     }
 
     /* Return whether the keyguard requires a password to unlock. */
-    public boolean lockScreenCheck(){
-        KeyguardManager km = (KeyguardManager)getSystemService(Activity.KEYGUARD_SERVICE);
+    public boolean getLockScreenStatus(KeyguardManager km){
         return km.isKeyguardSecure();
     }
-
-    /* Turn Lock Screen Off. */
-    public void setLockScreen(KeyguardManager.KeyguardLock lock, Boolean lock_screen){
-
-        if(lock_screen) {
-            //Log.d("Setting Lock Screen", "On");
-            lock.disableKeyguard();
-            lock.reenableKeyguard();
-        }
-        else if (!lock_screen){
-            //Log.d("Setting Lock Screen", "Off");
-             lock.disableKeyguard();
-        }
-    }
-
     public boolean getAutoSyncStatus(){
         return ContentResolver.getMasterSyncAutomatically();
 
     }
-
-    public void setAutoSyncStatus(Boolean setSync){
-        Log.d("Setting Auto Sync", setSync.toString());
-        ContentResolver.setMasterSyncAutomatically(setSync);
-    }
-
-
     public boolean getMobileDataStatus(){
 
         boolean mobileDataEnabled = false; // Assume disabled
@@ -151,33 +172,73 @@ public class MainActivity extends Activity {
         return mobileDataEnabled;
     }
 
+
+    /* Turn Lock Screen Off. */
+    public void setLockScreen(KeyguardManager.KeyguardLock lock, Boolean lock_screen){
+
+        if(lock_screen) {
+            //Log.d("Setting Lock Screen", "On");
+            lock.reenableKeyguard();
+        }
+        else if (!lock_screen){
+            //Log.d("Setting Lock Screen", "Off");
+             lock.disableKeyguard();
+        }
+    }
+
+    public void setAutoSyncStatus(Boolean setSync){
+        Log.d("Setting Auto Sync", setSync.toString());
+        ContentResolver.setMasterSyncAutomatically(setSync);
+    }
+
     public void setMobileDataStatus(Boolean setMobileData){
 
-        TelephonyManager telephonyManager = (TelephonyManager)
-                getSystemService(Context.TELEPHONY_SERVICE);
+        final ConnectivityManager cm = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
 
         try {
-            Class telephonyManagerClass = Class.forName(telephonyManager.getClass().getName());
-            Method getITelephonyMethod = telephonyManagerClass.getDeclaredMethod("getITelephony");
-            getITelephonyMethod.setAccessible(true);
-            Object ITelephonyStub = getITelephonyMethod.invoke(telephonyManager);
-            Class ITelephonyClass = Class.forName(ITelephonyStub.getClass().getName());
-            Method dataConnSwitchmethod = ITelephonyClass.getDeclaredMethod("");
-            dataConnSwitchmethod.setAccessible(true);
-            dataConnSwitchmethod.invoke(ITelephonyStub);
 
-            if (setMobileData) {
-                dataConnSwitchmethod = ITelephonyClass
-                        .getDeclaredMethod("disableDataConnectivity");
-            }
-            else if (!setMobileData) {
-                dataConnSwitchmethod = ITelephonyClass
-                        .getDeclaredMethod("enableDataConnectivity");
-            }
+            Class cmClass = Class.forName(cm.getClass().getName());
+            Field iConnectivityManagerField = cmClass.getDeclaredField("mService");
+            iConnectivityManagerField.setAccessible(true);
+            Object iConnectivityManager = iConnectivityManagerField.get(cm);
+            Class iConnectivityManagerClass =
+                    Class.forName(iConnectivityManager.getClass().getName());
+            Method setMobileDataEnabledMethod =
+                    iConnectivityManagerClass.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
+            setMobileDataEnabledMethod.setAccessible(true);
 
-        } catch (Exception e) {
-            Log.e("Error", e.toString());
+            setMobileDataEnabledMethod.invoke(iConnectivityManager, setMobileData);
+
         }
+        catch (NoSuchMethodException e) {
+            /*The method 'setMobileDataEnabled' been changed
+             * This catch tries to access the method again in Android 4.4.x */
+
+            try {
+                Class[] cArg = new Class[2];
+                cArg[0] = String.class;
+                cArg[1] = Boolean.TYPE;
+
+                Class cmClass = Class.forName(cm.getClass().getName());
+                Field iConnectivityManagerField = cmClass.getDeclaredField("mService");
+                iConnectivityManagerField.setAccessible(true);
+                Object iConnectivityManager = iConnectivityManagerField.get(cm);
+                Class iConnectivityManagerClass =
+                        Class.forName(iConnectivityManager.getClass().getName());
+                Method setMobileDataEnabledMethod =
+                        iConnectivityManagerClass.getDeclaredMethod("setMobileDataEnabled", cArg);
+
+                Object[] pArg = new Object[2];
+                pArg[0] = getPackageName();
+                pArg[1] = setMobileData;
+                setMobileDataEnabledMethod.setAccessible(true);
+                setMobileDataEnabledMethod.invoke(iConnectivityManager, pArg);
+            }
+            catch (Exception ee){Log.e("Error", e.toString());}
+
+        }
+        catch (Exception e) {Log.e("Error", e.toString());}
 
     }
 
