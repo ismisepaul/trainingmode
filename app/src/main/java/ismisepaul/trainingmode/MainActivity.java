234630c2fc63f36d05d701a15da5ce5efdde566b
ismisepaul <pmccann1984@gmail.com>
2014-07-22 15:26:59 +0100

    Removed settings menu

--- app/src/main/java/ismisepaul/trainingmode/MainActivity.java
+++ app/src/main/java/ismisepaul/trainingmode/MainActivity.java
@@ -126,7 +126,7 @@ public class MainActivity extends Activity {
         }
 
     }
-
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -145,7 +145,7 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-
+*/
     /* Return whether the keyguard requires a password to unlock. */
     public boolean getLockScreenStatus(KeyguardManager km){
         return km.isKeyguardSecure();
